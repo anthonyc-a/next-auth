179e3baef551213f3f0022cfa3b168bfabb13db3
anthonyc-a <anthonyc.animba@gmail.com>
2022-05-17 21:24:37 +0100

    added bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app"
 import "../styles/globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Button from "react-bootstrap/Button";
+import { ButtonGroup, Container, Toast } from "react-bootstrap";
 
 const Home = () => {
   const { data: session } = useSession();
-  console.log(session?.user)
+
+  const [showA, setShowA] = useState(true);
+
+  const toggleShowA = () => setShowA(!showA);
 
   return (
-    <div>
-      <a
-        href={`/api/auth/signin`}
-        onClick={(e) => {
-          e.preventDefault();
-          signIn();
-        }}
-      >
-        Sign in
-      </a>
-    </div>
+    <Container className="p-2">
+      <ButtonGroup className="me-4">
+        <Button className="me-2 rounded" onClick={toggleShowA}>
+          Show account details
+        </Button>
+        <Button
+          className="me-2 rounded"
+          onClick={(e) => {
+            e.preventDefault();
+            signIn();
+          }}
+        >
+          Sign in
+        </Button>
+        <Button
+        className="rounded"
+          onClick={(e) => {
+            e.preventDefault();
+            signOut();
+          }}
+        >
+          Sign out
+        </Button>
+      </ButtonGroup>
+
+      <Toast show={showA} onClose={toggleShowA}>
+        <Toast.Header>
+          <img
+            src={session?.user?.image!}
+            alt="avatar"
+            className="rounded me-2"
+          />
+          <strong className="me-auto">{session?.user?.name!}</strong>
+          <small>11 mins ago</small>
+        </Toast.Header>
+        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+      </Toast>
+
+      <img />
+    </Container>
   );
 };
 
